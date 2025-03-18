dc803edef2d03bf8d1311f0086aa62bc0a89ba65
ArnavTapadia <at565@cornell.edu>
2025-03-18 18:43:36 -0400

    reording params

--- Controllers/evaluate_control_cost.m
+++ Controllers/evaluate_control_cost.m
@@ -5,5 +5,5 @@ function weighted_cost = evaluate_control_cost(force_function, X0, params)
     [T, X, U] = simulate_pendulum(X0, force_function, params);
     
     % Compute control performance metrics
-    [weighted_cost, ~, ~, ~, ~, ~, ~] = compute_control_metrics(params, T, X, U);
+    [weighted_cost, ~, ~, ~, ~, ~, ~, ~] = compute_control_metrics(T, X, U, params);
 end
