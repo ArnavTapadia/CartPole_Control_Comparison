4cc7c91d8a8ae1ef76196f63e9667f4337fbf829
ArnavTapadia <at565@cornell.edu>
2025-03-23 22:36:08 -0400

    adding equation to line of best fit

--- Analysis/Param_Sweep_Functions/plot_best_fit_line.m
+++ Analysis/Param_Sweep_Functions/plot_best_fit_line.m
@@ -1,7 +1,34 @@
 function plot_best_fit_line(x, y, color)
 % Plots a linear best fit line through (x, y) in the specified color
-    coeffs = polyfit(x, y, 1);       % Linear fit
+% Also displays the fit equation on the plot
+
+    coeffs = polyfit(x, y, 1);  % [m, b]
     x_fit = linspace(min(x), max(x), 100);
     y_fit = polyval(coeffs, x_fit);
+
+    % Plot line
     plot(x_fit, y_fit, '-', 'Color', color, 'LineWidth', 1.2, 'HandleVisibility', 'off');
+
+    % Create equation string
+    m = coeffs(1);
+    b = coeffs(2);
+    eqn_str = sprintf('y = %.2fx %s %.2f', m, ternary(b >= 0, '+', '-'), abs(b));
+
+    % Add annotation near top-left of current subplot
+    ax = gca;
+    x_range = ax.XLim;
+    y_range = ax.YLim;
+    x_text = x_range(1) + 0.02 * range(x_range);
+    y_text = y_range(2) - 0.08 * range(y_range);
+    text(x_text, y_text, eqn_str, 'Color', color, ...
+        'FontSize', 10, 'Interpreter', 'none', 'FontWeight', 'bold');
+end
+
+function out = ternary(cond, val_true, val_false)
+% Helper function for inline conditional logic
+    if cond
+        out = val_true;
+    else
+        out = val_false;
+    end
 end
