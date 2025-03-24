e677785edb1d63aa677e7f4521abe55a08f99cd0
ArnavTapadia <at565@cornell.edu>
2025-03-23 23:05:44 -0400

    Increasing initial conditions and checking ± angle

--- Analysis/Param_Sweep_Functions/initial_conditions_sweep.m
+++ Analysis/Param_Sweep_Functions/initial_conditions_sweep.m
@@ -1,12 +1,29 @@
 % ------------------------
-% initial_conditions_sweep.m - Defines a variety of initial conditions we
-% will sweep through
+% initial_conditions_sweep.m - Generates a dense grid of initial conditions
 % ------------------------
 function ICs = initial_conditions_sweep()
-    ICs = {
-    [0; 0; 0.3; 0; 0],    % First is default for optimization
-    [0; 0; 0.2; 0; 0],
-    [0.5; 0; 0.2; 0; 0],
-    [0; 0; 0.1; 0.5; 0]
-    };
+
+    % Fixed cart position
+    x = 0;
+
+    % Sweep ranges (angle bounded within ±0.45 rad)
+    dx_vals     = linspace(-1.5, 1.5, 5);   % cart velocity
+    theta_vals  = linspace(-0.45, 0.45, 5); % pendulum angle
+    dtheta_vals = linspace(-2.5, 2.5, 5);   % angular velocity
+
+    % Build grid of combinations
+    ICs = {};
+    idx = 1;
+    for dx = dx_vals
+        for theta = theta_vals
+            for dtheta = dtheta_vals
+                ICs{idx} = [x; dx; theta; dtheta; 0];  % 5th entry for integral error
+                idx = idx + 1;
+            end
+        end
+    end
+
+    % Optional: place default optimization condition first
+    default_ic = [x; 0; 0.3; 0; 0];
+    ICs = [{default_ic}, ICs];
 end
--- Analysis/compare_same_control_runs.m
+++ Analysis/compare_same_control_runs.m
@@ -7,7 +7,7 @@ clear all;
 clc;
 
 %% Define initial conditions
-X0 = [0; 0; 0.1; 0; 0]; % Example initial conditions
+X0 = [0; 0; 0.3; 0; 0]; % Example initial conditions
 params = parameters;
 
 %% Create Figure for Motion Plots
@@ -15,20 +15,20 @@ fig_motion = figure('Name', 'Motion Plots');
 fig_metrics = figure('Name', 'Metrics');
 
 %% Run Simulation for PID Kp=50
-% Kp = 50; Ki = 105; Kd = 10;
-% force_function = @(t, X) pid_controller(t, X, Kp, Ki, Kd, params);
-% [T, X, U] = simulate_pendulum(X0, force_function, params);
-% motion_plots(fig_motion, T, X, 'PID Manual 0.1');
-% plot_control_metrics(fig_metrics, T, X, U, params, 'PID Manual 0.1');
-% 
-% 
-% X0 = [0; 0; 0.2; 0; 0]; % Example initial conditions
-% Kp = 50; Ki = 105; Kd = 10;
-% force_function = @(t, X) pid_controller(t, X, Kp, Ki, Kd, params);
-% [T, X, U] = simulate_pendulum(X0, force_function, params);
-% motion_plots(fig_motion, T, X, 'PID Manual 0.2');
-% plot_control_metrics(fig_metrics, T, X, U, params, 'PID Manual 0.2');
-% 
+Kp = 50; Ki = 105; Kd = 10;
+force_function = @(t, X) pid_controller(t, X, Kp, Ki, Kd, params);
+[T, X, U] = simulate_pendulum(X0, force_function, params);
+motion_plots(fig_motion, T, X, 'PID Manual 0.3');
+plot_control_metrics(fig_metrics, T, X, U, params, 'PID Manual 0.3');
+
+
+X0 = [0; 0; -0.3; 0; 0]; % Example initial conditions
+Kp = 50; Ki = 105; Kd = 10;
+force_function = @(t, X) pid_controller(t, X, Kp, Ki, Kd, params);
+[T, X, U] = simulate_pendulum(X0, force_function, params);
+motion_plots(fig_motion, T, X, 'PID Manual -0.3');
+plot_control_metrics(fig_metrics, T, X, U, params, 'PID Manual -0.3');
+
 % X0 = [0; 0; 0.3; 0; 0]; % Example initial conditions
 % Kp = 50; Ki = 105; Kd = 10;
 % force_function = @(t, X) pid_controller(t, X, Kp, Ki, Kd, params);
@@ -43,21 +43,21 @@ fig_metrics = figure('Name', 'Metrics');
 % motion_plots(fig_motion, T, X, 'PID Manual 0.4');
 % plot_control_metrics(fig_metrics, T, X, U, params, 'PID Manual 0.4');
 
-X0 = [0; 0; 0.2; 0; 0];
-
-for i = 1:5
-%% Run Simulation for LQR
-    params.m_p = 0.1*i;
-    params.m_c = 0.5*i; 
-    params = update_dependent_params(params);
-
-    params.I
-
-    Q_manual = diag([1, 1, 10, 100]); % Penalize theta + dtheta more
-    R_manual = 0.1;
-    K_manual = compute_lqr_gains(params, Q_manual, R_manual);
-    force_function = @(t, X) lqr_controller(t, X, K_manual, params);
-    [T, X, U] = simulate_pendulum(X0, force_function, params);
-    motion_plots(fig_motion, T, X, ['LQR m_p=' num2str(params.m_p) ' m_c= ' num2str(params.m_c)]);
-    plot_control_metrics(fig_metrics, T, X, U, params, ['LQR m_p=' num2str(params.m_p) ' m_c= ' num2str(params.m_c)]);
-end
+% X0 = [0; 0; 0.2; 0; 0];
+% 
+% for i = 1:5
+% %% Run Simulation for LQR
+%     params.m_p = 0.1*i;
+%     params.m_c = 0.5*i; 
+%     params = update_dependent_params(params);
+% 
+%     params.I
+% 
+%     Q_manual = diag([1, 1, 10, 100]); % Penalize theta + dtheta more
+%     R_manual = 0.1;
+%     K_manual = compute_lqr_gains(params, Q_manual, R_manual);
+%     force_function = @(t, X) lqr_controller(t, X, K_manual, params);
+%     [T, X, U] = simulate_pendulum(X0, force_function, params);
+%     motion_plots(fig_motion, T, X, ['LQR m_p=' num2str(params.m_p) ' m_c= ' num2str(params.m_c)]);
+%     plot_control_metrics(fig_metrics, T, X, U, params, ['LQR m_p=' num2str(params.m_p) ' m_c= ' num2str(params.m_c)]);
+% end
