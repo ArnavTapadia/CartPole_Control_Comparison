93ac539a97cd9ab5a8f5f33fb49f288ee7e6470f
ArnavTapadia <at565@cornell.edu>
2025-03-25 01:48:47 -0400

    running analysis

--- Analysis/Param_Sweep_Functions/param_sweeps_analysis.m
+++ Analysis/Param_Sweep_Functions/param_sweeps_analysis.m
@@ -19,36 +19,36 @@ plot_avg_metrics(T, x_vals, x_label);
 % 
 % 
 % %% ========== Sweep 2: Pendulum Length (L) ==========
-% params = parameters();
-% param_name = 'L';
-% param_values = linspace(0.1, 1, 10);  % m
-% 
-% results = run_param_sweep(param_name, param_values);
-% T = convert_param_sweep_results_to_table(results);
-% x_vals = param_values;
-% x_label = 'Pendulum Length L (m)';
-% plot_avg_metrics(T, x_vals, x_label);
-% 
-% 
-% %% ========== Sweep 3: Cart Fluid Friction (b) ==========
-% params = parameters();
-% param_name = 'b';
-% param_values = linspace(0, 1, 10);  % Ns/m
-% 
-% results = run_param_sweep(param_name, param_values);
-% T = convert_param_sweep_results_to_table(results);
-% x_vals = param_values;
-% x_label = 'Cart Fluid Friction b (Ns/m)';
-% plot_avg_metrics(T, x_vals, x_label);
+params = parameters();
+param_name = 'L';
+param_values = linspace(0.1, 1, 10);  % m
+
+results = run_param_sweep(param_name, param_values);
+T = convert_param_sweep_results_to_table(results);
+x_vals = param_values;
+x_label = 'Pendulum Length L (m)';
+plot_avg_metrics(T, x_vals, x_label);
 % 
 % 
-% %% ========== Sweep 4: Pivot Friction (mu_pivot) ==========
-% params = parameters();
-% param_name = 'mu_pivot';
-% param_values = linspace(0, 1, 10);  % Nms/rad
-% 
-% results = run_param_sweep(param_name, param_values);
-% T = convert_param_sweep_results_to_table(results);
-% x_vals = param_values;
-% x_label = 'Pivot Friction \mu_{pivot} (Nms/rad)';
-% plot_avg_metrics(T, x_vals, x_label);
+%% ========== Sweep 3: Cart Fluid Friction (b) ==========
+params = parameters();
+param_name = 'b';
+param_values = linspace(0, 1, 10);  % Ns/m
+
+results = run_param_sweep(param_name, param_values);
+T = convert_param_sweep_results_to_table(results);
+x_vals = param_values;
+x_label = 'Cart Fluid Friction b (Ns/m)';
+plot_avg_metrics(T, x_vals, x_label);
+
+
+%% ========== Sweep 4: Pivot Friction (mu_pivot) ==========
+params = parameters();
+param_name = 'mu_pivot';
+param_values = linspace(0, 1, 10);  % Nms/rad
+
+results = run_param_sweep(param_name, param_values);
+T = convert_param_sweep_results_to_table(results);
+x_vals = param_values;
+x_label = 'Pivot Friction \mu_{pivot} (Nms/rad)';
+plot_avg_metrics(T, x_vals, x_label);
